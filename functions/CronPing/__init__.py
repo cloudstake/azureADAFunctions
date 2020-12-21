05ea6bc6ec1bb30505a04757b98c71ab882280fd
PANS System <panssystem@protonmail.com>
2020-12-21 13:57:59 -0500

    Refactoring and planning for eventGrid.

--- functions/CronPing/__init__.py
+++ functions/CronPing/__init__.py
@@ -5,6 +5,10 @@ import os
 
 import azure.functions as func
 
+hosts = [
+    ("north-america.relays-new.cardano-mainnet.iohk.io",None,None),
+    ("north-america.relays-new.cardano-mainnet.iohk.io",None,None),
+]
 
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
@@ -21,7 +25,23 @@ def main(mytimer: func.TimerRequest) -> None:
     logging.info(os.getgid())
     logging.info(os.getgroups())
 
-    result = subprocess.run(["./bin/cncli", "ping","--host","north-america.relays-new.cardano-mainnet.iohk.io"],capture_output=True)
-    logging.info(result)
+    for host in hosts:
+        ping(host)
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
+def ping(host: string, port: int = None, magic: string = None):
+    args = ["./bin/cncli", "ping","--host",host]
+    if port != None:
+        args.append("--port")
+        args.append(port)
+    if magic != None:
+        args.append("--magic")
+        args.append(magic)
+    result = subprocess.run(args,capture_output=True)
+    out = json.loads(result.stdout);
+    if out.status != "ok":
+        report(out)
+
+def report(result):
+    
